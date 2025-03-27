602961eeec336170e68d05ca8a99831589ee9780
honghyowon <luckydnjs2@naver.com>
2025-03-27 20:27:19 +0900

    2025.03.27 Commit Test

---
+++ hellojava/src/ch04/sec08/paratice/Example6.java
@@ -0,0 +1,13 @@
+package ch04.sec08.paratice;
+
+public class Example6 {
+	public static void main(String[] args) {
+		for(int i=1; i<6; i++) {
+			
+			for(int j=1; j<=i; j++) {				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
